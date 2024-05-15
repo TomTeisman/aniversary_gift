a53f683858892eeb23c0179adff4c135347075fc
TomTeisman <tjg.teisman@gmail.com>
2024-05-15 16:20:07 +0200

    update

--- js/index.js
+++ js/index.js
@@ -18,21 +18,69 @@ const assets = [
             src: "assets/01_begin/eerst_bij_mij.jpg",
             aspectRatio: "3 / 4"
         },
-        description: ""
-    }
+        description: "De eerste keer dat je bij mij thuis was"
+    },
+    {
+        image: {
+            src: "assets/01_begin/park_1.jpg",
+            aspectRatio: "375 / 667"
+        },
+        description: "De dagen dat we samen in het park zaten..."
+    },
+    {
+        image: {
+            src: "assets/01_begin/park_2.jpg",
+            aspectRatio: "36 / 61"
+        },
+        description: "De dagen dat we samen in het park zaten..."
+    },
+    {
+        image: {
+            src: "assets/01_begin/park_3.jpg",
+            aspectRatio: "725 / 1291"
+        },
+        description: "De dagen dat we samen in het park zaten..."
+    },
+    {
+        image: {
+            src: "assets/01_begin/park_4.jpg",
+            aspectRatio: "9 / 16"
+        },
+        description: "De dagen dat we samen in het park zaten..."
+    },
 ];
 
 var i = 0;
 
-function nextPicture() {
+function initialPicture() {
     document.getElementById("photo").src = assets[i].image.src;
     document.getElementById("photo-wrapper").style.aspectRatio = assets[i].image.aspectRatio;
     
     document.getElementById("description").innerHTML = assets[i].description;
+}
+
+function nextPicture() {
+    if (i >= (assets.length - 1)) {
+        return;
+    }
+
     i++;
+
+    document.getElementById("photo").src = assets[i].image.src;
+    document.getElementById("photo-wrapper").style.aspectRatio = assets[i].image.aspectRatio;
+    
+    document.getElementById("description").innerHTML = assets[i].description;
 }
 
 function previousPicture() {
+    if (i === 0) {
+        return;
+    }
+
     i--;
-    document.getElementById("photo").src = assets[i].img_src;
+
+    document.getElementById("photo").src = assets[i].image.src;
+    document.getElementById("photo-wrapper").style.aspectRatio = assets[i].image.aspectRatio;
+
+    document.getElementById("description").innerHTML = assets[i].description;
 }
