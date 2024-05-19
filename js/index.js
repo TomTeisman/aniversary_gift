b06deabefc6400589b340b96451cc4fbabb96ce3
TomTeisman <tjg.teisman@gmail.com>
2024-05-19 21:59:00 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -1,86 +1,256 @@
-const assets = [
-    {
-        image: {
-            src: "assets/01_begin/chat.jpg",
-            aspectRatio: "375 / 667"
+const chapters = [
+    [
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/chat.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: "Weet je nog hoe ik je in het begin perongelijk 3 dagen skipte... Ik ben wel echt ongeloofelijk blij wat hieruit is gekomen."
         },
-        description: "Weet je nog hoe ik je in het begin perongelijk 3 dagen skipte...<br>Ik ben wel echt ongeloofelijk blij wat hieruit is gekomen."
-    },
-    {
-        image: {
-            src: "assets/01_begin/eerste_date.jpg",
-            aspectRatio: "1539 / 2048"
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/eerste_date.jpg",
+                aspectRatio: "1539 / 2048"
+            },
+            description: "Onze eerste date, de dag waarop de discussie over de (water)vogels ontstond en waarop je mijn haar gestijld had net als dat van jou.<br><br>Daarnaast natuurlijk onze eerste kus<3"
         },
-        description: "Onze eerste date"
-    },
-    {
-        image: {
-            src: "assets/01_begin/eerst_bij_mij.jpg",
-            aspectRatio: "3 / 4"
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/eerst_bij_mij.jpg",
+                aspectRatio: "3 / 4"
+            },
+            description: "De eerste keer dat je bij mij thuis was"
         },
-        description: "De eerste keer dat je bij mij thuis was"
-    },
-    {
-        image: {
-            src: "assets/01_begin/park_1.jpg",
-            aspectRatio: "375 / 667"
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/park_1.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: "De dagen dat we samen in het park zaten..."
         },
-        description: "De dagen dat we samen in het park zaten..."
-    },
-    {
-        image: {
-            src: "assets/01_begin/park_2.jpg",
-            aspectRatio: "36 / 61"
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/park_2.jpg",
+                aspectRatio: "36 / 61"
+            },
+            description: "De dagen dat we samen in het park zaten..."
         },
-        description: "De dagen dat we samen in het park zaten..."
-    },
-    {
-        image: {
-            src: "assets/01_begin/park_3.jpg",
-            aspectRatio: "725 / 1291"
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/park_3.jpg",
+                aspectRatio: "725 / 1291"
+            },
+            description: "De dagen dat we samen in het park zaten..."
         },
-        description: "De dagen dat we samen in het park zaten..."
-    },
-    {
-        image: {
-            src: "assets/01_begin/park_4.jpg",
-            aspectRatio: "9 / 16"
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/park_4.jpg",
+                aspectRatio: "9 / 16"
+            },
+            description: "De dagen dat we samen in het park zaten..."
         },
-        description: "De dagen dat we samen in het park zaten..."
-    },
+        {
+            title: "Hoe het begon",
+            image: {
+                src: "assets/01_begin/eerste_nummer.jpg",
+                aspectRatio: "1 / 1"
+            },
+            description: "Het eerste nummer wat ik voor je gezongen heb was Viva La Vida, ik weet nog dat ik best zenuwachtig was toen ik begon maar dat werd al snel minder toen ik zag hoe blij je was."
+        }
+    ],
+
+    [
+        {
+            title: "",
+            image: {
+                src: "assets/kis.gif",
+                aspectRatio: "1 / 1"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/park_1.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: "Deze foto is al heel lang eem vam mijn favorieten, hij was gemaakt op 13 juni 2023 (denk ik 🙃). Toen hadden we 2 weken. We lagen samen op de berg naar muziek te luisteren en ik weet nog heel goed dat ik steeds terug keek en dacht aan hoe mooi je bent."
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/park_3.jpg",
+                aspectRatio: "725 / 1334"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/park_2.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/bowlen.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/glowgolf.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/kermis.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/vapiano.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/rik.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/carnaval.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        },
+        {
+            title: "Onze momenten samen",
+            image: {
+                src: "assets/02_momenten/efteling.jpg",
+                aspectRatio: "375 / 667"
+            },
+            description: ""
+        }
+    ],
+
+    [
+        {
+            title: "",
+            image: {
+                src: "assets/love.gif",
+                aspectRatio: "5 / 4"
+            },
+            description: ""
+        },
+        {
+            title: "Hoe blij je me maakt",
+            image: {
+                src: "",
+                aspectRatio: ""
+            },
+            description: ""
+        },
+        {
+            title: "Hoe blij je me maakt",
+            image: {
+                src: "",
+                aspectRatio: ""
+            },
+            description: ""
+        },
+        {
+            title: "Hoe blij je me maakt",
+            image: {
+                src: "",
+                aspectRatio: ""
+            },
+            description: ""
+        },
+        {
+            title: "Hoe blij je me maakt",
+            image: {
+                src: "",
+                aspectRatio: ""
+            },
+            description: ""
+        },
+        {
+            title: "Hoe blij je me maakt",
+            image: {
+                src: "",
+                aspectRatio: ""
+            },
+            description: ""
+        }
+    ]
 ];
 
+var chapter = 0;
 var i = 0;
 
 function initialPicture() {
-    document.getElementById("photo").src = assets[i].image.src;
-    document.getElementById("photo-wrapper").style.aspectRatio = assets[i].image.aspectRatio;
+    document.getElementById("photo").src = chapters[chapter][i].image.src;
+    document.getElementById("photo-wrapper").style.aspectRatio = chapters[chapter][i].image.aspectRatio;
     
-    document.getElementById("description").innerHTML = assets[i].description;
+    document.getElementById("title").innerHTML = chapters[chapter][i].title;
+    document.getElementById("description").innerHTML = chapters[chapter][i].description;
 }
 
 function nextPicture() {
-    if (i >= (assets.length - 1)) {
-        return;
+    if (i >= (chapters[chapter].length - 1)) {
+        chapter++;
+        i = 0;
+    } else {
+        i++;
     }
 
-    i++;
 
-    document.getElementById("photo").src = assets[i].image.src;
-    document.getElementById("photo-wrapper").style.aspectRatio = assets[i].image.aspectRatio;
+    document.getElementById("photo").src = chapters[chapter][i].image.src;
+    document.getElementById("photo-wrapper").style.aspectRatio = chapters[chapter][i].image.aspectRatio;
     
-    document.getElementById("description").innerHTML = assets[i].description;
+    document.getElementById("title").innerHTML = chapters[chapter][i].title;
+    document.getElementById("description").innerHTML = chapters[chapter][i].description;
 }
 
 function previousPicture() {
     if (i === 0) {
-        return;
+        if (chapter === 0) {
+            return;
+        } else {
+            chapter--;
+            i = chapters[chapter].length - 1;
+        }
+    } else {
+        i--;
     }
-
-    i--;
-
-    document.getElementById("photo").src = assets[i].image.src;
-    document.getElementById("photo-wrapper").style.aspectRatio = assets[i].image.aspectRatio;
-
-    document.getElementById("description").innerHTML = assets[i].description;
+    
+    document.getElementById("photo").src = chapters[chapter][i].image.src;
+    document.getElementById("photo-wrapper").style.aspectRatio = chapters[chapter][i].image.aspectRatio;
+    
+    document.getElementById("title").innerHTML = chapters[chapter][i].title;
+    document.getElementById("description").innerHTML = chapters[chapter][i].description;
 }
